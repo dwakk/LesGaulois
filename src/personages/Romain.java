43a9c51470cb7088aaff30f8723cbcadc12d2323
dwakk <111376922+dwakk@users.noreply.github.com>
2025-10-07 09:36:48 +0200

    TP 2 partie 3

--- src/personages/Romain.java
+++ src/personages/Romain.java
@@ -1,8 +1,12 @@
 package personages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		super();
@@ -39,8 +43,32 @@ public class Romain {
 		return force >= 0 ? true : false;
 	}
 
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2: {
+			System.out.println("Le soldat " + nom + " est déjà bien protégé!");
+			break;
+		}
+		case 1: {
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + "!");
+				break;
+			}
+		}
+		default:
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + "!");
+			equipements[nbEquipement] = equipement;
+			nbEquipement++;
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
 		Romain minus = new Romain("minus", 6);
 		assert minus.isInvariantVerified();
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
